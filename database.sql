b9a41c92101f8dc002583f606dce4aa3d088b1f1
jbazans <jbazans@uni.pe>
2020-03-11 20:43:14 -0500

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE PRODUCTO(
+	codpro int not null AUTO_INCREMENT,
+	nompro varchar(50) null,
+	despro varchar(100) null,
+	prepro number(6,2) null,
+	estado int null,
+	CONSTRAINT pk_producto
+	PRIMARY KEY (codpro)
+);
+
+alter table PRODUCTO add rutimapro varchar(100) null;
+
+INSERT INTO PRODUCTO (nompro,despro,prepro,estado,rutimapro)
+VALUES ('Papel Crepe','Ideal para decoraci&oacute;n de trabajos escolares','14.99',1,'crepe.jpg')
+,('Papel Bond A4','Papel ultra blanco de 180gr','9.99',1,'bonda4.jpg');
